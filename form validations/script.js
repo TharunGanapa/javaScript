0f63f730745db719ac993a3498f9be4519e5d160
Tharun Ganapa <ganapatharun@gmail.com>
2026-04-22 09:44:16 +0530

    added content

--- form validations/script.js
+++ form validations/script.js
@@ -1,18 +1,36 @@
-const validateForm = (event) => {
-    event.preventDefault()
+// const validateForm = (event) => {
+//     event.preventDefault()
 
-    let username = event.target.username.value
-    let password = event.target.password.value
-    let msg = document.getElementById("message")
+//     let username = event.target.username.value
+//     let password = event.target.password.value
+//     let msg = document.getElementById("message")
 
 
-    if (username === "" && password === ""){
-        msg.innerText = "please Enter"
+//     if (username === "" && password === ""){
+//         msg.innerText = "please Enter"
+//     }
+//     else if(username === ""){
+//         msg.innerText = "please enter username";
+//     }
+//     else if(password === ""){
+//         msg.innerText = "please enter password";
+//     }
+// }
+
+const fetchAPI = async () => {
+    let imageURL = null;
+    try {
+
+        let res = await fetch("https://dog.ceo/api/breeds/image/random")
+        let data = await res.json()
+        imageURL = data.message
+    } catch (err) {
+        console.log(err);
+        
     }
-    else if(username === ""){
-        msg.innerText = "please enter username";
-    }
-    else if(password === ""){
-        msg.innerText = "please enter password";
-    }
+
+    let image = document.getElementById("image")
+    image.src = imageURL
 }
+
+fetchAPI()
